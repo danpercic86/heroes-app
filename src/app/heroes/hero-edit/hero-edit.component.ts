41b5b6e00cc0567bb09c70a2579ee21004cf04cc
Dan Percic <dan@devplant.ro>
2022-04-13 22:05:18 +0300

    use a form group

--- src/app/heroes/hero-edit/hero-edit.component.ts
+++ src/app/heroes/hero-edit/hero-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from '../models/hero.model';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HeroesApiService } from '../heroes-api.service';
 import { Subscription } from 'rxjs';
 
@@ -12,35 +12,36 @@ import { Subscription } from 'rxjs';
 })
 export class HeroEditComponent {
   hero: Hero;
-  name: FormControl;
-  city: FormControl;
-  website = new FormControl('', [Validators.required]);
+  form: FormGroup;
   subscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private heroesApiService: HeroesApiService, private router: Router) {
+  constructor(private route: ActivatedRoute, private heroesApiService: HeroesApiService, private router: Router, private formBuilder: FormBuilder) {
     this.hero = this.route.snapshot.data['hero'];
     /**
      * Use the constructor of FormControl to set its initial value, which in this case sets hero's values.
      * By creating these controls in your component class, you get immediate access to listen for, update, and validate the state of the form input.
      */
-    this.name = new FormControl(this.hero.name);
-    this.city = new FormControl(this.hero.city);
-    this.website.setValue(this.hero.website);
-
-    this.subscription = this.name.valueChanges.subscribe(value => {
-      this.hero.name = value;
+    this.form = this.formBuilder.group({
+      id: [this.hero.id],
+      name: [this.hero.name, [Validators.required, Validators.minLength(3)]],
+      city: [this.hero.city, [Validators.required, Validators.minLength(3)]],
+      website: ['', [Validators.required, Validators.minLength(3)]],
     });
+
+    this.subscription = this.form.valueChanges.subscribe(value => {
+      console.log(value);
+    });
+    this.subscription.add(this.form.get('name')?.valueChanges.subscribe(value => {
+      this.hero.name = value;
+    }));
   }
 
-  get isValid() {
-    return this.name.valid && this.city.valid && this.website.valid;
+  get websiteHasErrors() {
+    return this.form.get('website')?.hasError('required') && this.form.get('website')?.touched;
   }
 
   save() {
-    this.hero.name = this.name.value;
-    this.hero.city = this.city.value;
-    this.hero.website = this.website.value;
-    this.heroesApiService.updateHero(this.hero);
+    this.heroesApiService.updateHero(this.form.getRawValue() as Hero);
     this.subscription.unsubscribe();
     return this.router.navigate(['/heroes']);
   }
