1ff2c2b710f9d3595a5f87d9e44a2dad59877e01
Dan Percic <dan@devplant.ro>
2022-04-13 16:28:25 +0300

    declare components in exactly one module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
+    /**
+     * Every component must be declared in exactly one NgModule.
+     * Angular CLI by default declared HeroesComponent in the AppModule when it generated that component.
+     */
     HeroesComponent,
   ],
   imports: [
