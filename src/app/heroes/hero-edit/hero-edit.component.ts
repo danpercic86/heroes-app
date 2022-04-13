58e23228eea2004a91fceeeda64a5a89cf6cae42
Dan Percic <dan@devplant.ro>
2022-04-13 21:48:28 +0300

    use validators for controls

--- src/app/heroes/hero-edit/hero-edit.component.ts
+++ src/app/heroes/hero-edit/hero-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Hero } from '../models/hero.model';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { HeroesApiService } from '../heroes-api.service';
 import { Subscription } from 'rxjs';
 
@@ -14,7 +14,7 @@ export class HeroEditComponent {
   hero: Hero;
   name: FormControl;
   city: FormControl;
-  website: FormControl;
+  website = new FormControl('', [Validators.required]);
   subscription: Subscription;
 
   constructor(private route: ActivatedRoute, private heroesApiService: HeroesApiService, private router: Router) {
@@ -25,11 +25,15 @@ export class HeroEditComponent {
      */
     this.name = new FormControl(this.hero.name);
     this.city = new FormControl(this.hero.city);
-    this.website = new FormControl(this.hero.website);
+    this.website.setValue(this.hero.website);
 
     this.subscription = this.name.valueChanges.subscribe(value => {
       this.hero.name = value;
     });
+  }
+
+  get isValid() {
+    return this.name.valid && this.city.valid && this.website.valid;
   }
 
   save() {
