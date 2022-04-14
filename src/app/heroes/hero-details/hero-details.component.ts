b1025de6b733ba739845bcf6a8d73b5efd25d894
Dan Percic <dan@devplant.ro>
2022-04-14 19:25:13 +0300

    limiting service scope within components

--- src/app/heroes/hero-details/hero-details.component.ts
+++ src/app/heroes/hero-details/hero-details.component.ts
@@ -3,11 +3,17 @@ import { HeroesApiService } from '../heroes-api.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, Observable } from 'rxjs';
 import { Hero } from '../models/hero.model';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.scss'],
+  /**
+   * The providers array from components work the same way as the providers array from modules,
+   * except that they are only available to the component and its children.
+   */
+  providers: [AppService],
 })
 export class HeroDetailsComponent {
   readonly hero$: Observable<Hero>;
@@ -16,7 +22,12 @@ export class HeroDetailsComponent {
     private route: ActivatedRoute,
     private router: Router,
     private heroesApiService: HeroesApiService,
+    /**
+     * This instance ao AppService is different from the one from HeroesModule and the one from global scope
+     */
+    appService: AppService,
   ) {
+    console.log(appService.message);
     /**
      * Get an observable for heroId so that we will be notified when it changes.
      */
